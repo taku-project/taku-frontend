f1b89ecf058ad3bf3158c43585111ccd50ed2898
KingNono1030 <hoejy0821@gmail.com>
2024-12-11 22:07:12 +0900

    feat: 임시 페이지 추가

---
+++ src/pages/community/UpdateCommunityPage.tsx
@@ -0,0 +1,5 @@
+const UpdateCommunityPage = () => {
+  return <div>커뮤니티 수정 페이지</div>;
+};
+
+export default UpdateCommunityPage;
---
+++ src/pages/market/MarketDetailPage.tsx
@@ -0,0 +1,5 @@
+const CommunityDetailPage = () => {
+  return <div>커뮤니티 상세 페이지</div>;
+};
+
+export default CommunityDetailPage;
