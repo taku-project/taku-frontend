372230c768a5c901f83f22a30b5421c4eb63cedb
KingNono1030 <hoejy0821@gmail.com>
2024-12-11 12:01:49 +0900

    chore: prettier plugin (import 정렬, tailwind 클래스명 정렬) 설치 및 적용

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -7,4 +7,17 @@ module.exports = {
   useTabs: false, // 스페이스 사용
   bracketSpacing: true, // 객체 리터럴에서 중괄호 사이에 공백 추가
   arrowParens: 'always', // 화살표 함수의 매개변수에 괄호 항상 추가
+  endOfLine: 'lf', // Unix 스타일 개행
+  importOrder: [
+    '^react$', // React는 항상 맨 위에 위치
+    '^@?\\w', // 외부 라이브러리
+    '^(@/.*)$', // 절대 경로 import
+    '^[./]', // 상대 경로 import
+  ],
+  importOrderSeparation: true, // import 그룹 간 줄바꿈 추가
+  importOrderSortSpecifiers: true, // import된 항목 알파벳 순서로 정렬
+  plugins: [
+    '@trivago/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss',
+  ], // Trivago 및 TailwindCSS 플러그인 추가
 };
