6a0a5f26cdda2d540e23efad7b554cb7b4aee29a
KingNono1030 <hoejy0821@gmail.com>
2024-12-11 11:17:20 +0900

    chore: eslint-plugin-react 설치 및 lint config 에 반영

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
@@ -14,8 +15,14 @@ export default tseslint.config(
       globals: globals.browser,
     },
     plugins: {
+      'react' : react,
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      'react-refresh': reactRefresh
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -23,6 +30,13 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      'react/function-component-definition': [
+        'warn',
+        {
+          namedComponents: 'arrow-function',
+        },
+      ],
+      'react/react-in-jsx-scope': 'off',
     },
   },
 )
