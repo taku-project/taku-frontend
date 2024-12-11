4038d1f1527a43447388cf9ed574c471552edc22
KingNono1030 <hoejy0821@gmail.com>
2024-12-11 11:40:36 +0900

    chore: prettier 설치 및 eslint config 에 prettier 관련 규칙 추가

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true, // 세미콜론 사용
+  singleQuote: true, // 작은따옴표 사용
+  trailingComma: 'all', // 여러 줄에서 항상 쉼표 사용
+  printWidth: 80, // 한 줄 최대 길이
+  tabWidth: 2, // 탭 너비
+  useTabs: false, // 스페이스 사용
+  bracketSpacing: true, // 객체 리터럴에서 중괄호 사이에 공백 추가
+  arrowParens: 'always', // 화살표 함수의 매개변수에 괄호 항상 추가
+};
--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,29 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      'plugin:prettier/recommended',
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      'react' : react,
+      react,
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh
+      'react-refresh': reactRefresh,
+      prettier,
     },
     settings: {
       react: {
@@ -37,6 +43,7 @@ export default tseslint.config(
         },
       ],
       'react/react-in-jsx-scope': 'off',
+      'prettier/prettier': 'warn',
     },
   },
-)
+);
