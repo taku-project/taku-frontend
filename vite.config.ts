8e689295df9881f41d52318703997379642da4d6
KingNono1030 <hoejy0821@gmail.com>
2024-12-11 12:58:52 +0900

    chore: vite-checker 설치 및 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({
+      typescript: true, // TypeScript만 검사
+    }),
+  ],
 });
