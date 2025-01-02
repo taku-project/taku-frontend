3b4a613602113e4d9859f894d05e78e2e2e3aa8a
KingNono1030 <hoejy0821@gmail.com>
2025-01-02 22:30:51 +0900

    feat: auth service 메서드 카카오 로그인 추가 (임시)

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -4,3 +4,19 @@ export const signIn = async (data: { email: string; password: string }) => {
   const response = await ducku.post('', data);
   return response;
 };
+export const signUp = async (data: {
+  nickname: string;
+  phoneNumber: string;
+  authCode: string;
+}) => {
+  const response = await ducku.post('', data);
+  return response;
+};
+
+export const kakaoLogin = async (data: {
+  code: string;
+  redirectURL: string;
+}) => {
+  const response = await ducku.post('', data);
+  return response;
+};
