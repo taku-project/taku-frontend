be383ab9010f51eaccf1415471808adc6f2a85bb
KingNono1030 <hoejy0821@gmail.com>
2025-01-13 22:51:05 +0900

    feat: axios 인스턴스에 공통 에러 핸들링 로직 추가

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -7,4 +7,33 @@ const ducku = axios.create({
   },
 });
 
+/**
+ * 요청 인터셉터
+ */
+
+ducku.interceptors.request.use(
+  (config) => {
+    // 요청 전에 공통 처리 (예: 인증 헤더 추가)
+    return config;
+  },
+  (error) => {
+    console.error('axios 요청 중 에러:', error);
+    return Promise.reject(error);
+  },
+);
+
+/**
+ * 응답 인터셉터
+ */
+
+ducku.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    console.error('axios 응답 중 에러:', error);
+    return Promise.reject(error);
+  },
+);
+
 export default ducku;
