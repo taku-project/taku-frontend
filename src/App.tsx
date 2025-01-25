5fd832f379f0d3743b381e689936f10fe8540b14
KingNono1030 <hoejy0821@gmail.com>
2025-01-25 23:13:04 +0900

    feat: google oauth provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Suspense } from 'react';
 
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { BrowserRouter } from 'react-router-dom';
 
+import { GOOGLE_CLIENT_ID } from './constants/api/oauth';
 import ScrollToTop from './layout/ScrollToTop';
 import Router from './routes';
 
@@ -13,14 +15,16 @@ const queryClient = new QueryClient();
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Suspense>
-          <ScrollToTop />
-          <Router />
-        </Suspense>
-      </BrowserRouter>
-      {/* The rest of your application */}
-      <ReactQueryDevtools initialIsOpen={false} />
+      <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+        <BrowserRouter>
+          <Suspense>
+            <ScrollToTop />
+            <Router />
+          </Suspense>
+        </BrowserRouter>
+        {/* The rest of your application */}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </GoogleOAuthProvider>
     </QueryClientProvider>
   );
 };
